c7b978dcf8ae5de215e2c47923deab8d7151354d
Taran Mahajan <137894393+txrxn10@users.noreply.github.com>
2024-07-15 20:06:53 +0530

    Create CB_Task_1.cpp

---
+++ CB_Task_1.cpp
@@ -0,0 +1,17 @@
+﻿#include "CB_Task_1_Sandbox.h"
+
+int main(int argc, char* argv[])
+{
+	init();
+	std::thread t_1(threadCalls);
+	_delay_ms(4000);
+	
+	//e_shape();
+	//Task_1_1();
+	Task_1_2();
+	
+	////////////////////////////////////////////////////////////////////////////////////////////
+	
+	cleanUp();
+	t_1.detach();
+}
